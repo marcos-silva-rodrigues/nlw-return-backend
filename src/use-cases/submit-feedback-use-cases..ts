ddf8ac0baf58f47bfd3d156175b6ec31b9d0acef
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-05-07 20:11:45 -0300

    feat: correct order in validation and sending screenshot in email

--- src/use-cases/submit-feedback-use-cases..ts
+++ src/use-cases/submit-feedback-use-cases..ts
@@ -15,23 +15,24 @@ export class SubmitFeedbackUseCase {
 
   async execute(request: SubmitFeedbackUseCaseRequest) {
     const { type, comment, screenshot } = request;
+
+    if (!type) {
+      throw new Error("Type is Required");
+    }
+
+    if (!comment) {
+      throw new Error("Comment is Required");
+    }
+
+    if (screenshot && !screenshot.startsWith("data:image/png;base64")) {
+      throw new Error("Invalid screenshot format.");
+    }
+
     await this.feedbackRepository.create({
       type,
       comment,
       screenshot,
     });
-
-    if (!type) {
-      throw new Error('Type is Required');
-    }
-
-    if (!comment) {
-      throw new Error('Comment is Required');
-    }
-
-    if (screenshot && !screenshot.startsWith('data:image/png;base64')) {
-      throw new Error('Invalid screenshot format.');
-    }
 
     await this.mailAdapter.sendEmail({
       subject: "Novo feedback",
@@ -39,6 +40,7 @@ export class SubmitFeedbackUseCase {
         `<div style="font-family: san-serif; font-size: 16px; color: #111;">`,
         `<p>Tipo do feedback: ${type}<p/>`,
         `<p>Comentario: ${comment}<p/>`,
+        screenshot ? `<img src="${screenshot}"` : "",
         `<div />`,
       ].join("\n"),
     });
